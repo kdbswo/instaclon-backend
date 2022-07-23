42c0ae59cde25d926df733d64ba0a02cbf2f16a3
kdbswo <kdbswo@gmail.com>
2022-07-24 05:00:14 +0900

    Revise

--- schema.js
+++ schema.js
@@ -3,7 +3,7 @@ import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
 import { makeExecutableSchema } from "apollo-server";
 
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
-const loadResolvers = loadFilesSync(`${__dirname}/**/*.{queries,mutation}.js`);
+const loadResolvers = loadFilesSync(`${__dirname}/**/*.{queries,mutations}.js`);
 
 const typeDefs = mergeTypeDefs(loadedTypes);
 const resolvers = mergeResolvers(loadResolvers);
