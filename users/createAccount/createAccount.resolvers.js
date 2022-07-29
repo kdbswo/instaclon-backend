944832bd42a7ee58b6d1cd8d6c52164d54ed457c
kdbswo <kdbswo@gmail.com>
2022-07-29 09:32:45 +0900

    divide createAccount

---
+++ users/createAccount/createAccount.resolvers.js
@@ -0,0 +1,41 @@
+import bcrypt from "bcrypt";
+import client from "../client";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      try {
+        const existingUser = await client.user.findFirst({
+          where: {
+            OR: [
+              {
+                username,
+              },
+              {
+                email,
+              },
+            ],
+          },
+        });
+        if (existingUser) {
+          throw new Error("This username or password is already taken.");
+        }
+        const uglyPassword = await bcrypt.hash(password, 10); //보안
+        return client.user.create({
+          data: {
+            username,
+            email,
+            firstName,
+            lastName,
+            password: uglyPassword,
+          },
+        });
+      } catch (e) {
+        return e;
+      }
+    },
+  },
+};
--- users/users.mutations.js
+++ users/users.mutations.js
@@ -4,40 +4,6 @@ import client from "../client";
 
 export default {
   Mutation: {
-    createAccount: async (
-      _,
-      { firstName, lastName, username, email, password }
-    ) => {
-      try {
-        const existingUser = await client.user.findFirst({
-          where: {
-            OR: [
-              {
-                username,
-              },
-              {
-                email,
-              },
-            ],
-          },
-        });
-        if (existingUser) {
-          throw new Error("This username or password is already taken.");
-        }
-        const uglyPassword = await bcrypt.hash(password, 10); //보안
-        return client.user.create({
-          data: {
-            username,
-            email,
-            firstName,
-            lastName,
-            password: uglyPassword,
-          },
-        });
-      } catch (e) {
-        return e;
-      }
-    },
     login: async (_, { username, password }) => {
       const user = await client.user.findFirst({ where: { username } });
       if (!user) {
