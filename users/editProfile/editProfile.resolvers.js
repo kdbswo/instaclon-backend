b48a3529e749ede72f94e750392d6e4ab9e6975a
kdbswo <kdbswo@gmail.com>
2022-08-09 05:59:21 +0900

    recap#2

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -2,41 +2,41 @@ import bcrypt from "bcrypt";
 import client from "../../client";
 import { protectedResolver } from "../users.utils";
 
+const resolverFn = async (
+  _,
+  { firstName, lastName, username, email, password: newPassword },
+  { loggedInUser, protectResolver }
+) => {
+  let uglyPassword = null;
+  if (newPassword) {
+    uglyPassword = await bcrypt.hash(newPassword, 10);
+  }
+  const updatedUser = await client.user.update({
+    where: {
+      id: loggedInUser.id,
+    },
+    data: {
+      firstName,
+      lastName,
+      username,
+      email,
+      ...(uglyPassword && { password: uglyPassword }), // uglyPassword가 참이면 password: uglyPassword 를 반환
+    },
+  });
+  if (updatedUser.id) {
+    return {
+      ok: true,
+    };
+  } else {
+    return {
+      ok: false,
+      error: "Could not update profile.",
+    };
+  }
+};
+
 export default {
   Mutation: {
-    editProfile: protectedResolver(
-      async (
-        _,
-        { firstName, lastName, username, email, password: newPassword },
-        { loggedInUser, protectResolver }
-      ) => {
-        let uglyPassword = null;
-        if (newPassword) {
-          uglyPassword = await bcrypt.hash(newPassword, 10);
-        }
-        const updatedUser = await client.user.update({
-          where: {
-            id: loggedInUser.id,
-          },
-          data: {
-            firstName,
-            lastName,
-            username,
-            email,
-            ...(uglyPassword && { password: uglyPassword }), // uglyPassword가 참이면 password: uglyPassword 를 반환
-          },
-        });
-        if (updatedUser.id) {
-          return {
-            ok: true,
-          };
-        } else {
-          return {
-            ok: false,
-            error: "Could not update profile.",
-          };
-        }
-      }
-    ),
+    editProfile: protectedResolver(resolverFn),
   },
 };
