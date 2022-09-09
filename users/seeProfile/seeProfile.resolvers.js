fe0e0a451036d4f374fcecdfc3c4b235c876a1cc
kdbswo <kdbswo@gmail.com>
2022-09-09 09:20:04 +0900

    computed Fields #1

--- users/seeProfile/seeProfile.resolvers.js
+++ users/seeProfile/seeProfile.resolvers.js
@@ -7,6 +7,7 @@ export default {
         where: {
           username,
         },
+        //include 원하는 사용자 관계를 갖고 올 수 있게 해줌 (사용자가 적을때 사용)
         include: {
           following: true,
           followers: true,
