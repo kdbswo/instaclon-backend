a4b9bda1b1b4ec660cdb43619d48d46a2f2f66b8
kdbswo <kdbswo@gmail.com>
2022-10-02 18:43:07 +0900

    searchusers pagination

--- users/searchUsers/searchUsers.resolvers.js
+++ users/searchUsers/searchUsers.resolvers.js
@@ -2,13 +2,27 @@ import client from "../../client";
 
 export default {
   Query: {
-    searchUsers: async (_, { keyword }) =>
-      client.user.findMany({
+    searchUsers: async (_, { keyword, page }) => {
+      const searchedUsers = await client.user.findMany({
         where: {
           username: {
             startsWith: keyword.toLowerCase(),
           },
         },
-      }),
+        take: 5,
+        skip: (page - 1) * 5,
+      });
+      const totalsearch = await client.user.count({
+        where: {
+          username: {
+            startsWith: keyword.toLowerCase(),
+          },
+        },
+      });
+      return {
+        searchedUsers,
+        totalSearchPages: Math.ceil(totalsearch / 5),
+      };
+    },
   },
 };
--- users/searchUsers/searchUsers.typeDefs.js
+++ users/searchUsers/searchUsers.typeDefs.js
@@ -1,7 +1,11 @@
 import { gql } from "apollo-server";
 
 export default gql`
+  type SearchUsersResult {
+    searchedUsers: [User]
+    totalSearchPages: Int
+  }
   type Query {
-    searchUsers(keyword: String!): [User]
+    searchUsers(keyword: String!, page: Int!): SearchUsersResult!
   }
 `;
