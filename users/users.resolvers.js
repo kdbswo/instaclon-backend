c9aa42ad74f16e870901df186dd4ede25e893c96
kdbswo <kdbswo@gmail.com>
2022-10-06 04:21:46 +0900

    editPhoto

--- users/users.resolvers.js
+++ users/users.resolvers.js
@@ -49,5 +49,11 @@ export default {
       });
       return Boolean(exists);
     },
+    photos: ({ id }) =>
+      client.user
+        .findUnique({
+          where: { id },
+        })
+        .photos(),
   },
 };
