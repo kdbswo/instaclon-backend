cad3e4edb12214cc77a7c2ed34999f0fda85e1bf
kdbswo <kdbswo@gmail.com>
2022-10-17 22:19:31 +0900

    filter #3 onConnect

--- messages/roomUpdates/roomUpdates.resolvers.js
+++ messages/roomUpdates/roomUpdates.resolvers.js
@@ -7,8 +7,15 @@ export default {
   Subscription: {
     roomUpdates: {
       subscribe: async (root, args, context, info) => {
-        const room = await client.room.findUnique({
-          where: { id: args.id },
+        const room = await client.room.findFirst({
+          where: {
+            id: args.id,
+            users: {
+              some: {
+                id: context.loggedInUser.id,
+              },
+            },
+          },
           select: { id: true },
         });
         if (!room) {
@@ -16,8 +23,24 @@ export default {
         }
         return withFilter(
           () => pubsub.asyncIterator(NEW_MESSAGE),
-          ({ roomUpdates }, { id }) => {
-            return roomUpdates.roomId === id;
+          async ({ roomUpdates }, { id }, { loggedInUser }) => {
+            const room = await client.room.findFirst({
+              where: {
+                id,
+                users: {
+                  some: {
+                    id: loggedInUser.id,
+                  },
+                },
+              },
+              select: { id: true },
+            });
+            if (roomUpdates.roomId === id) {
+              if (!room) {
+                return false;
+              }
+              return true;
+            }
           }
         )(root, args, context, info);
       },
