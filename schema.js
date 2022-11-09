35b5859ad5afff4d2f71ddf2e7a8edbe5905334c
kdbswo <kdbswo@gmail.com>
2022-11-09 16:05:40 +0900

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,6 +1,5 @@
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
-import { makeExecutableSchema } from "@graphql-tools/schema";
 
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
 const loadResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.js`);
